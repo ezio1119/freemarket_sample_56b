177711f31e93cfdde9a4ac49f0059778ed67b9c1
Katsutoshi Hayashi <ezio1119@gmail.com>
2019-08-08 14:03:44 +0900

    Update application_controller.rb
    BASIC認証を追加
    本番のみ適用

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth, if: :production? 
+  protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
-
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :first_name, :last_name, :birth_date])
+  end
+
+  private
+  def production?
+    Rails.env.production?
+  end
+  
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == Rails.application.credentials.basic[:BASIC_AUTH_USER] && password == Rails.application.credentials.basic[:BASIC_AUTH_PASSWORD]
+    end
   end
 
 end
