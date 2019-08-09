e9ef811b91991aea1e31b148a5a60d7ccc8e5ecd
hizukayudai@gmail.com <09120912Abcd>
2019-08-09 21:20:15 +0900

    update migration

--- db/schema.rb
+++ db/schema.rb
@@ -33,21 +33,7 @@ ActiveRecord::Schema.define(version: 2019_08_09_114713) do
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
   end
 
-  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
-    t.integer "size"
-    t.text "name", null: false
-    t.integer "delivery", null: false
-    t.integer "region", null: false
-    t.integer "days", null: false
-    t.integer "price", null: false
-    t.text "info", null: false
-    t.bigint "user_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_items_on_user_id"
-  end
-
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "nickname", null: false
@@ -66,5 +52,4 @@ ActiveRecord::Schema.define(version: 2019_08_09_114713) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "items", "users"
 end
