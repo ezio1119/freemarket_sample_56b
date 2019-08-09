f0b2a97ab184831b1644762de97d4c8d8c2a61ff
hizukayudai@gmail.com <09120912Abcd>
2019-08-09 15:48:15 +0900

    Update address view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords',
+  }
+
+  devise_scope :user do
+    get 'users/address' => 'users/registrations#address'
+    get 'users/index' => 'users/registrations#index'
+  end
+
   root 'items#index'
   get 'users/profile' => 'users#profile'
 
