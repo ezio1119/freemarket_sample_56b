27a99ade1f4b02c51711543eaa7c3e48c391c5af
hizukayudai@gmail.com <09120912Abcd>
2019-08-09 15:48:15 +0900

    Update address view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
   def show
-    
+  end
+
+  def index
+    binding.pry
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,19 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords',
+  }
+
+  devise_scope :user do
+    get 'users/address' => 'users/registrations#address'
+    get 'users/index' => 'users/registrations#index'
+  end
+
   root 'items#index'
   resources :items, only: [:index, :show]
-  resources :users, only:[:show, :index]
+  resources :users, only:[:show]
+
 end
 
