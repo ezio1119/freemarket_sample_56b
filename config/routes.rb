a5164f4cf75faa71e4bbbbedd4397e56a17bcd56
nabec <nabec.48@gmail.com>
2019-08-11 17:21:27 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
   root 'items#index'
   get 'users/profile' => 'users#profile'
   get 'users/identification' => 'users#identification'
-  resources :items, only: [:index, :show, :new]
+  resources :items, only: [:index, :show, :new] do
+    member do
+      get :buy
+    end
+  end
   resources :users, only:[:show, :index]
   resources :cards, only: :index
 end
