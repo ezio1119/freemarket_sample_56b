bd45519fd1b40c5d9aa2b00f60073bb2d23b9641
fujisawatk <fuji19880707@gmail.com>
2019-08-14 02:53:21 +0900

    add destroy method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,46 +1,55 @@
 class ItemsController < ApplicationController
-
-    def index
-      @items = Item.all
-    end
-
-    def show
-      @item = Item.find(params[:id])
-    end
-    
-    def new
-      @item = Item.new
-    end
-
-    def create
-      @item = Item.new(item_params)
-      if @item.save
-      redirect_to root_path
-      else
-        render :new
-      end
-    end
   
-    def buy
-      
-    end   
+  def index
+    @items = Item.all
+  end
 
-    def edit
-      @item = Item.with_attached_image.find(params[:id])
+  def show
+    @item = Item.find(params[:id])
+  end
+  
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+    redirect_to root_path
+    else
+      render :new
     end
+  end
 
-    def update
-      @item = Item.find(params[:id])
-      if @item.update(item_params)
-        redirect_to item_path(@item)
-      else
-        render :edit
-      end
+  def buy
+    
+  end   
+
+  def edit
+    @item = Item.with_attached_image.find(params[:id])
+  end
+
+  def update
+    @item = Item.with_attached_image.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+    else
+      render :edit
     end
+  end
 
-    private
-
-    def item_params
-      params.require(:item).permit(:name, :state_id, :delivery_burden_id, :prefecture_id,:delivery_method_id, :day_id, :price, :info, :image)
+  def destroy
+    @item = Item.with_attached_image.find(params[:id])
+    binding.pry
+    if @item.user_id == current_user.id
+      @item.destroy!
+      redirect_to items_list_users_path
     end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :state_id, :delivery_burden_id, :prefecture_id,:delivery_method_id, :day_id, :price, :info, :image).merge(user_id: current_user.id)
+  end
 end
