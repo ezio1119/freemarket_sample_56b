17b11876341ee2b1acac5f138e460d73a3d72880
fujisawatk <fuji19880707@gmail.com>
2019-08-14 17:55:06 +0900

    Add Omniauth column to Users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_16_021308) do
+ActiveRecord::Schema.define(version: 2019_08_14_084956) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -110,6 +110,8 @@ ActiveRecord::Schema.define(version: 2019_08_16_021308) do
     t.string "first_name_kana", null: false
     t.string "last_name_kana", null: false
     t.string "payjp_cus"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
