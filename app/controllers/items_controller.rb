ea14074885179582ad4b96fc5e589543a53c858b
hizukayudai@gmail.com <09120912Abcd>
2019-08-18 16:29:12 +0900

    Items-controller-test

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,21 +11,21 @@ class ItemsController < ApplicationController
   def show
   end
   
+
   def new
     @item = Item.new
   end
 
   def create
     @item = Item.new(item_params)
-      if @item.save
+    if @item.save
       redirect_to root_path
-      else
-        render :new
-      end
+    else
+      render :new
+    end
   end
 
   def buy
-    
   end   
 
   def search
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     nickname                {"テスト太郎"}
-    email                   {"kkk@gamil.com"}
+    sequence(:email) {Faker::Internet.email}
     password                {"00000000"}
     password_confirmation   {"00000000"}
     first_name              {"田中"}
