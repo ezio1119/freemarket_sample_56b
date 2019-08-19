a86c9c375057136c1a87b4a757738539fb90efaa
nabec <nabec.48@gmail.com>
2019-08-19 17:22:18 +0900

    Update breadcrumbs.rb
    add link item.name

--- config/breadcrumbs.rb
+++ config/breadcrumbs.rb
@@ -2,6 +2,11 @@ crumb :root do
   link "メルカリ", root_path
 end
 
+crumb :item_show do |item|
+  link "#{item.name}"
+  parent :root
+end
+
 crumb :mypage do
   link "マイページ", user_path
   parent :root
