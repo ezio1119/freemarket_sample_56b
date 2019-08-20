72efc1a5fe9230345ba146aeb6c345f5064f6246
fujisawatk <fuji19880707@gmail.com>
2019-08-20 12:59:07 +0900

    add feature test of registration page

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,8 @@ require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
+require 'devise'
+require 'capybara/rspec'
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -59,5 +61,9 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
-  config.include Devise::Test::ControllerHelpers, type: :controller
+  # for Capybara : Capybara用のDSLをinclude
+  config.include Capybara::DSL
+
+  # for Devise : Deviseを使用している時にsign in/outを取るためのヘルパーをinclude
+  include Warden::Test::Helpers
 end
