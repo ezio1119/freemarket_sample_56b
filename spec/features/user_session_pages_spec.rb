1a28bb8dee97c846df1956cc306a4d201f407a83
fujisawatk <fuji19880707@gmail.com>
2019-08-20 12:59:35 +0900

    add feature test of session page

---
+++ spec/features/user_session_pages_spec.rb
@@ -0,0 +1,33 @@
+require 'rails_helper'
+
+feature "user signin/signout" do
+
+  let(:user) { create(:user) }
+
+    before { visit root_path }
+
+  scenario "can signin" do
+    visit new_user_session_path
+    fill_in "user_email", with: user.email
+    fill_in "user_password", with: user.password
+    find('input[name="commit"]').click
+    expect(current_path).to eq root_path
+    expect(page).not_to have_link('signin')
+  end
+
+  scenario "can not signin" do
+    visit new_user_session_path
+    fill_in "user_email", with: 'other@example.com'
+    fill_in "user_password", with: 'other'
+    find('input[name="commit"]').click
+    expect(current_path).to eq new_user_session_path
+  end
+
+  scenario "user sign out" do
+    logout(:user)  
+
+    expect(current_path).to eq root_path
+    expect(page).not_to have_link('signout')
+    expect(page).to have_http_status(200)
+  end
+end
