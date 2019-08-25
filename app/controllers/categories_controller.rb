90c6ba7556dd9f9ea515621876d7aebe891e6561
nabec <nabec.48@gmail.com>
2019-08-25 12:14:16 +0900

    Update categories_contoroller.rb

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -11,14 +11,21 @@ class CategoriesController < ApplicationController
   
   def show
     @category = Category.find(params[:id])
-    @categories = [
-      # 下記で@categoryを取得
-      @category,
-      # 下記で@categoryの子カテゴリーを全て取得
-      @category.children,
-      # 下記で@categoryの子カテゴリーの子カテゴリーを全て取得
-      @category.children.map { |category| category.children }
-    ].flatten.compact
+    if params[:id].to_i <= 13
+      @categories = [@category.children.map { |category| category.children }].flatten.compact
+    elsif params[:id].to_i <= 159
+      @categories = [@category.children].flatten.compact
+    else
+      @categories = [@category].flatten.compact
+    end
+
+    items = []
+    @categories.each do |category|
+      category.items.each do |item|
+        items << item
+      end
+    end
+    @items = Kaminari.paginate_array(items).page(params[:page]).per(1)
   end
 
 end
