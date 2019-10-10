f9aefebdc79b216d95a2394308f790f905a13267
hizukayudai@gmail.com <09120912Abcd>
2019-10-10 21:20:06 +0900

    Update category_items.rb

--- app/controllers/concerns/category_items.rb
+++ app/controllers/concerns/category_items.rb
@@ -1,12 +1,12 @@
 module CategoryItems
   extend ActiveSupport::Concern
-  
-  btm_ids = [[*(160..339)], [*(340..470)], [*(471..586)], [*(587..714)], [*(715..765)], [*(766..866)], [*(867..954)], [*(955..1029)], [*(1030..1125)], [*(1126..1167)], [*(1168..1218)], [*(1219..1274)], [*(1275..1331)]]
+
   def category_items(category)
     if category.id <= 13
+      btm_ids = [[*(160..339)], [*(340..470)], [*(471..586)], [*(587..714)], [*(715..765)], [*(766..866)], [*(867..954)], [*(955..1029)], [*(1030..1125)], [*(1126..1167)], [*(1168..1218)], [*(1219..1274)], [*(1275..1331)]]
       ids = btm_ids[category.id - 1]
       items = Item.where(category_id: ids).order("created_at DESC")
-      exit
+      return items
     elsif category.id <= 159
       categories = [category.children].flatten.compact
     else
